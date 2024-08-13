829beb4733768883087ce05f7594bf0631fd9054
samiksha014 <158751878+samiksha014@users.noreply.github.com>
2024-08-14 00:02:18 +0530

    count.py

--- python_practical3/count.py
+++ python_practical3/count.py
@@ -2,17 +2,25 @@
 
 #first approach 
 def count_fun(string,sub_string,flag):
+    # num identifier will carry the count of the non overlapping sub strin
+    num = 0
     
     #counts non overlapping sub strings in the given string if the flag is set to false
     if flag==False:
-        # c identifier will carry the count of the non overlapping sub string
-        c = string.count('gg')
-        return c
+        i = 0
         
-    else:
-        # the count of the sub strings 
-        num = 0
-        
+        while i < len(string)-1:
+            if string[i] + string[i+1] == 'gg':
+                num += 1
+                
+                #this will iterate over next to check the non-overlapping of the sub string
+                i += 2
+                
+            else:
+                # otherwise the iterator will iterate by 1
+                i += 1
+   
+    else:   
         # for loop to avoid the string length go out of bounds 
         for i in range(len(string)-1):
            if string[i] + string[i+1] == 'gg':

b5e97ad8520232d1f62920a8bc7e5bfb13728782
samiksha014 <158751878+samiksha014@users.noreply.github.com>
2024-08-14 22:58:03 +0530

    modulo.py

--- python_practical3/modulo.py
+++ python_practical3/modulo.py
@@ -1,4 +1,21 @@
-#create a modulo function which performs '%' operation of two numbers
-# def modulo(numerator,denominator)
-#pass every possible combination of numbers
+#numerator = dividend
+#denominator = divisor
+
+def modulo(numerator,denominator):
+    if denominator == 0:
+        return "Undefined Operation"
+
+    
+    #to get acurate floating point resukt we use float division here
+    result = numerator // denominator
+    if result >= 0:
+        var = int(result)
+    else:
+        var = int(result) - 1
+    mod = numerator - (var * denominator)
+    return mod
+
+num = float(input())
+deno = float(input())
+print(modulo(num,deno))
 
